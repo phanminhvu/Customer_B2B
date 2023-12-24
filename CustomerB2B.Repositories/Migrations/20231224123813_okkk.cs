c4a83938c203e6864b030d5eee6e9231d8072232
Eric Dang <ken174@outlook.com>
2023-12-24 19:38:40 +0700

    a

---
+++ CustomerB2B.Repositories/Migrations/20231224123813_okkk.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CustomerB2B.Repositories.Migrations
+{
+    public partial class okkk : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CompanySystemInformations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CustomerType = table.Column<short>(type: "smallint", nullable: true),
+                    SaleStaff = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CompanyId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompanySystemInformations", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CompanySystemInformations");
+        }
+    }
+}
